1b9656d76eeb011319c73aad11f5874741f5aa14
leozdgao <gzdleo0816@gmail.com>
2015-12-15 17:52:06 +0800

    handle parallax scrolling

--- sites/_components/banner/index.js
+++ sites/_components/banner/index.js
@@ -1,4 +1,5 @@
 import style from 'dom-helpers/style'
+import parallax from 'lgutil/lib/dom/parallax'
 import { preloadBkgImage, fromArray, curriedAddClass } from '../../_utils/utils'
 import dataset from '../../_utils/dataset'
 
@@ -6,12 +7,23 @@ import dataset from '../../_utils/dataset'
 const bannerImages = fromArray(document.querySelectorAll('.banner-image'))
 bannerImages.forEach(bannerImage => {
   const bkgUrl = dataset(bannerImage, 'url')
+  const needParallax = dataset(bannerImage, 'parallax')
   if (bkgUrl) {
     preloadBkgImage(bkgUrl, _ => {
       style(bannerImage, {
         "background-image": `url(${bkgUrl})`
       })
       curriedAddClass('show')(bannerImage)
+
+      // 仅在大屏幕下使用视差滚动
+      if (needParallax) {
+        if (document.documentElement.clientWidth > 1050) {
+          parallax(0.1)(bannerImage)
+          style(bannerImage, {
+            "background-attachment": "fixed"
+          })
+        }
+      }
     })
   }
 })
